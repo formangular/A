74a791a212be71f934b41df84d7e3e1f8953f5d9
formangular <56075330+formangular@users.noreply.github.com>
2019-10-02 14:55:49 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+# s3.zylowski.net/public/input/6.txt
+
+"""
+[Student A] Menu of application
+[Student B] Downloading text file from the internet
+[Student A] Count letters
+[Student B] Count words
+[Student C] Count punctuation marks
+[Student C] Count sentences
+[Student A] Generate report about count of every letter in the file
+[Student C] Save statistics to the file
+[Student B] Close the application
+git push -u origin master
+cat .git/config
+https://github.com/zwodek01/test.git
+"""
+
+
+def main():
+    menu()
+
+
+def menu():
+    print('**********Import Menu**********')
+
+
+main()
